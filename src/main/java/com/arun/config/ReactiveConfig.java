0d01349206c4ff7cdbe7457472fd7cffff7d41dc
strider90arun <strider90arun@gmail.com>
2017-06-15 17:53:30 -0700

    Upgrade to Spring Boot 2.0

--- src/main/java/com/arun/config/ReactiveConfig.java
+++ src/main/java/com/arun/config/ReactiveConfig.java
@@ -1,10 +1,14 @@
 package com.arun.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.arun")
+@EnableAutoConfiguration
 public class ReactiveConfig {
 	
 	public static void main(String[] args) {
